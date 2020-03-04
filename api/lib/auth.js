e1fb38c5ccc58d2be0c102100b3bab1b2d9adc97
Ryan Davidson <rydavidson@accela.com>
2020-03-04 14:33:24 -0700

    Use local

--- api/lib/auth.js
+++ api/lib/auth.js
@@ -7,7 +7,7 @@ const logger = require('./logger.js');
 exports.authenticateUser = function (username, hashedPassword, callback) {
 
   if(process.env.AUTH_MODE === "local"){
-    authenticateUserLocal(username, password, callback);
+    this.authenticateUserLocal(username, password, callback);
   }
 
   user.getUser(username, function (userObj) {
